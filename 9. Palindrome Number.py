e238c074c27f8ac63e1041b27e737ab1655fa59e
Dyson <Weaver1990@163.com>
2016-11-21 18:37:39 +0800

    easy with python

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,28 @@
+﻿"""
+Determine whether an integer is a palindrome. Do this without extra space.
+"""
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+
+        if len(s) == 1:
+            return True
+            
+        if len(s) % 2 == 0:
+            s1 = s[:len(s)/2]
+            s2 = s[len(s)/2:][::-1]
+            print s1
+            print s2
+        else:
+            s1 = s[:int(len(s)/2)]
+            s2 = s[int(len(s)/2) + 1:][::-1]
+        if s1 == s2:
+            return True
+        else:
+            return False
+

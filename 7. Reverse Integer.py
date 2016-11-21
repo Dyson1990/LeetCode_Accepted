4e560f81849820ce857a48d88feb2b6154b1e653
Dyson <Weaver1990@163.com>
2016-11-21 18:37:06 +0800

    easy with python

---
+++ 7. Reverse Integer.py
@@ -0,0 +1,21 @@
+﻿"""
+Reverse digits of an integer.
+
+Example1: x = 123, return 321
+Example2: x = -123, return -321 
+"""
+
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
+            return 0
+        sign = x / abs(x) # 正负号
+        s = str(abs(x))
+        s = s[::-1]
+        return int(s) * sign
+
